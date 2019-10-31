c7ea68822a534830813ffdd94720fbef07798594
eunyoungroh <roheunyoung@outlook.com>
2019-10-31 16:03:15 +0900

    실습 5

--- 09_array/09_array/Source.c
+++ 09_array/09_array/Source.c
@@ -2,63 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ROWS 3
-#define COLS 3
-
-void matrix_increment(int A[][COLS], int B[][COLS], int C[][COLS]);
-void print_matrix(int C[][COLS]);
-
 int main(void)
 {
-	int A[ROWS][COLS] = {
-		{2, 3, 0},
-		{8, 9, 1},
-		{7, 0, 5}
-	};
-
-	int B[ROWS][COLS] = {
-		{1, 0, 0},
-		{0, 1, 0},
-		{0, 0, 1}
-	};
-
-	int C[ROWS][COLS] = {
-		{0, 0, 0},
-		{0, 0, 0},
-		{0, 0, 0}
-	};
-
-	matrix_increment(A, B, C);
-	print_matrix(C);
-
-	return 0;
-}
-
-void matrix_increment(int A[][COLS], int B[][COLS], int C[][COLS])
-{
+	int s[3][5];
 	int i, j;
-	for (i = 0; i < ROWS; i++)
+	int value = 0;
+
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < COLS; j++)
+		for (j = 0; j < 5; j++)
 		{
-			C[i][j] = A[i][j] + B[i][j];
+			s[i][j] = value++;
 		}
 	}
 
-}
-
-void print_matrix(int C[][COLS])
-{
-	int i, j;
-
-	for (i = 0; i < ROWS; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < COLS; j++)
+		for (j = 0; j < 5; j++)
 		{
-			printf("%d ", C[i][j]);
+			printf("%d ", s[i][j]);
 		}
 		printf("\n");
 	}
+
+
+	return 0;
 }
 
 
